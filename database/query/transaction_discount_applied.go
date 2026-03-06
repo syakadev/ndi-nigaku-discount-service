b4d9b0803a03f0c4fa3bbe3f0e2f4781185c64fe
syakadev <syakadev@gmail.com>
2026-03-07 01:16:22 +0700

    update query transaction discount applied

--- database/query/transaction_discount_applied.go
+++ database/query/transaction_discount_applied.go
@@ -5,22 +5,20 @@ func ListTransactionDiscountApplied() string {
 	return `
 		SELECT
 			id,
-			discount_target_id,
-			target_id,
+			discount_transaction_target_id,
+			target_id, -- transaksi id target
 			price_before_discount,
 			total_discount,
 			price_after_discount,
 			created_at,
-			created_by,
-			updated_at,
-			updated_by
+			created_by
 		FROM
-			public.ndi_transaction_discount_applied
+			public.ndi_discount_transaction_applied
 		WHERE (
 			$1 = '' OR
-			price_before_discount ILIKE '%' || $1 || '%' OR
-			total_discount ILIKE '%' || $1 || '%' OR
-			price_after_discount ILIKE '%' || $1 || '%'
+			price_before_discount::text = $1 OR
+			total_discount::text ILIKE '%' || $1 || '%' OR
+			price_after_discount::text ILIKE '%' || $1 || '%'
 		) AND
 			deleted_at IS NULL
 		ORDER BY created_at DESC
@@ -34,34 +32,49 @@ func CountListTransactionDiscountApplied() string {
 		SELECT
 			COUNT(*)
 		FROM
-			public.ndi_applied_discount
+			public.ndi_discount_transaction_applied
 		WHERE (
 			$1 = '' OR
-			price_before_discount ILIKE '%' || $1 || '%' OR
-			total_discount ILIKE '%' || $1 || '%' OR
-			price_after_discount ILIKE '%' || $1 || '%'
+			price_before_discount::text = $1 OR
+			total_discount::text ILIKE '%' || $1 || '%' OR
+			total_discount::text ILIKE '%' || $1 || '%' OR
+			price_after_discount::text ILIKE '%' || $1 || '%'
+		) AND
+			deleted_at IS NULL;
+	`
+}
+
+func CountListTransactionDiscountAppliedByDiscountID() string {
+	return `
+		SELECT
+			COUNT(*)
+		FROM
+			public.ndi_discount_transaction_applied
+		WHERE (
+			discount_transaction_target_id = $1
 		) AND
 			deleted_at IS NULL;
 	`
 }
 
 // GetDiscountByID retrieves a single discount record
-func GetTransctionDiscountAppliedID() string {
+func GetListTransctionDiscountAppliedByDiscountID() string {
 	return `
 		SELECT
 			id,
-			discount_target_id,
+			discount_transaction_target_id,
+			target_id, -- transaksi id target
 			price_before_discount,
 			total_discount,
 			price_after_discount,
 			created_at,
-			created_by,
-			updated_at,
-			updated_by
+			created_by
 		FROM
-			public.ndi_applied_discount
+			public.ndi_discount_transaction_applied
 		WHERE
-			discount_id = $1 AND deleted_at IS NULL;
+			discount_transaction_target_id = $1
+		AND
+			deleted_at IS NULL
 		ORDER BY created_at DESC
 		LIMIT $2 OFFSET $3;
 	`
@@ -70,24 +83,20 @@ func GetTransctionDiscountAppliedID() string {
 // CreateDiscount inserts a new discount record
 func CreateTransactionDiscountApplied() string {
 	return `
-		INSERT INTO public.ndi_applied_discount (
-			discount_target_id,
+		INSERT INTO public.ndi_discount_transaction_applied (
+			discount_transaction_target_id,
+			target_id, -- transaksi id target
 			price_before_discount,
 			total_discount,
 			price_after_discount,
-			created_at,
-			created_by,
-			updated_at,
-			updated_by
+			created_by
 		) VALUES (
-		    $1,  -- discount_target_id
-			$2,  -- price_before_discount
-			$3,  -- total_discount
-			$4,  -- price_after_discount
-			NOW(), -- created_at
-			$5,  -- created_by
-			NOW(), -- updated_at
-			$6   -- updated_by
+			$1,  -- discount_transaction_target_id
+			$2,  -- target_id
+			$3,  -- price_before_discount
+			$4,  -- total_discount
+			$5,  -- price_after_discount
+			$6  -- created_by
 		);
 	`
 }
@@ -96,7 +105,7 @@ func CreateTransactionDiscountApplied() string {
 func DeleteTransactionDiscountApplied() string {
 	return `
 		UPDATE
-			public.ndi_applied_discount
+			public.ndi_discount_transaction_applied
 		SET
 			deleted_at = NOW(),
 			deleted_by = $2
